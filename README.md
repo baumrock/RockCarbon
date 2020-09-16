3c067b10ba21e678826511feceb9bd11c7c33367
BernhardBaumrock <office@baumrock.com>
2020-09-16 18:16:16 +0200

    init

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # RockCarbon
+
+## Usage
+
+```php
+$modules->get('RockCarbon')->load();
+db(\Carbon\Carbon::now('UTC'));
+
+// or like this
+use \Carbon\Carbon;
+db(Carbon::now('UTC'));
+```
+
+![img](https://i.imgur.com/q1UMQ4k.png)
+
+## Docs
+
+See https://carbon.nesbot.com/docs/
---
+++ RockCarbon.module.php
@@ -0,0 +1,28 @@
+<?php namespace ProcessWire;
+/**
+ * A simple PHP API extension for DateTime
+ * Using https://carbon.nesbot.com/
+ *
+ * @author Bernhard Baumrock, 15.09.2020
+ * @license Licensed under MIT
+ * @link https://www.baumrock.com
+ */
+class RockCarbon extends WireData implements Module, ConfigurableModule {
+
+  public static function getModuleInfo() {
+    return [
+      'title' => 'RockCarbon',
+      'version' => '0.0.1',
+      'summary' => 'A simple PHP API extension for DateTime',
+      'autoload' => false,
+      'singular' => true,
+      'icon' => 'clock-o',
+      'requires' => [],
+      'installs' => [],
+    ];
+  }
+
+  public function load() {
+    require_once('vendor/autoload.php');
+  }
+}
